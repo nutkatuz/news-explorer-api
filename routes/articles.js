17cdd1a3215d0b9231cc27a540c4a67fb4ae8ad5
Anna <nutkatuz@yandex.ru>
2020-12-22 15:21:40 +0500

    feat: beautiful joi msg

--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,5 @@
 const articleRouter = require('express').Router();
+const { isCelebrateError } = require('celebrate');
 const celebrateValid = require('../middlewares/celebrateValid');
 
 const {
@@ -8,7 +9,14 @@ const {
 } = require('../controllers/articles.js');
 
 articleRouter.get('/articles', getArticles);
-articleRouter.post('/articles', celebrateValid.postArticle, postArticle);
+articleRouter.post('/articles',
+  celebrateValid.postArticle,
+  (err, req, res, next) => {
+    if (!isCelebrateError(err)) return next(err);
+    const errorBody = err.details.get('body');
+    return res.status(400).send({ message: errorBody.message });
+  }, postArticle);
+
 articleRouter.delete('/articles/:_id', celebrateValid.deleteArticle, deleteArticle);
 
 module.exports = articleRouter;
