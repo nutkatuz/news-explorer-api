fb2d676b6a45e9a3ef8e83ff95f184e629f51934
Anna <nutkatuz@yandex.ru>
2021-02-15 13:24:52 +0500

    bugfix: disable celebrateValidation for articles

--- routes/articles.js
+++ routes/articles.js
@@ -1,6 +1,6 @@
 const articleRouter = require('express').Router();
 const { isCelebrateError } = require('celebrate');
-const celebrateValid = require('../middlewares/celebrateValid');
+// const celebrateValid = require('../middlewares/celebrateValid');
 
 const {
   getArticles, // возвращает все сохранённые пользователем статьи
@@ -10,13 +10,15 @@ const {
 
 articleRouter.get('/articles', getArticles);
 articleRouter.post('/articles',
-  celebrateValid.postArticle,
+  // celebrateValid.postArticle,
   (err, req, res, next) => {
     if (!isCelebrateError(err)) return next(err);
     const errorBody = err.details.get('body');
     return res.status(400).send({ message: errorBody.message });
   }, postArticle);
 
-articleRouter.delete('/articles/:_id', celebrateValid.deleteArticle, deleteArticle);
+articleRouter.delete('/articles/:_id',
+// celebrateValid.deleteArticle,
+  deleteArticle);
 
 module.exports = articleRouter;
